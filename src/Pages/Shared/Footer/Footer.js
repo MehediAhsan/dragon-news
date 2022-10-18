1851481e9f626ba136598ba0a0abdf8ae870bc03
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-10-18 21:11:22 +0600

    project setup

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>copyright newsportal</small></p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeLBG-0zGUqbVyMOPzvMKFRHADb7NBooI",
+  authDomain: "dragon-news-6451a.firebaseapp.com",
+  projectId: "dragon-news-6451a",
+  storageBucket: "dragon-news-6451a.appspot.com",
+  messagingSenderId: "339473973363",
+  appId: "1:339473973363:web:4115fe5aa6cfc65499aa06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
